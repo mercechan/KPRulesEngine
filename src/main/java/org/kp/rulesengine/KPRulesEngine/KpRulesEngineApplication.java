0dbe4ebcea4d602e212cbe2abe8c208a6412d5b6
Mercy Chan <mercy.chan@kp.org>
2019-12-17 14:44:10 -0800

    initial commit of kp rules engine code

---
+++ src/main/java/org/kp/rulesengine/KPRulesEngine/KpRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package org.kp.rulesengine.KPRulesEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpRulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpRulesEngineApplication.class, args);
+	}
+
+}
